9fe053daacf55d24460d73d41394feb4b6a306f2
Tom Buckley <buckley.tom@gmail.com>
2017-04-27 09:23:36 -0700

    line spacing fix

--- python/preprocess_gtfs_folders.py
+++ python/preprocess_gtfs_folders.py
@@ -11,11 +11,8 @@ for org in org_acronyms:
 	stop_times_file = "C:/temp/RegionalTransitDatabase/data/gtfs/{}/stop_times.txt".format(org)
 	stop_times_temp_db = "C:/temp/RegionalTransitDatabase/data/gtfs/{}/temp_db".format(org)
 	try:
-  		arcpy.transit.PreprocessStopTimes(stop_times_file, stop_times_temp_db)
-  		arcpy.transit.SimpleInterpolation(stop_times_temp_db, stop_times_file)
+		arcpy.transit.PreprocessStopTimes(stop_times_file, stop_times_temp_db)
+		arcpy.transit.SimpleInterpolation(stop_times_temp_db, stop_times_file)
 	except:
 		failures.append(org) 
-  		pass
-
-
-	
+		pass
