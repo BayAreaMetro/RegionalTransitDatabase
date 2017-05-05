44ee1bc09f85ed935123444f60d201fad89b934f
Tom Buckley <buckley.tom@gmail.com>
2017-05-05 12:43:17 -0700

    preliminary refactor of 2016 process.sql
    this is probably the resolution to https://github.com/MetropolitanTransportationCommission/RegionalTransitDatabase/issues/3.

---
+++ sql/process/step_14_clean_up_staging_tables.sql
@@ -0,0 +1,17 @@
+--Cleanup unneeded tables
+------------------------------------------------------------
+Print 'Cleanup unneeded tables'
+------------------------------------------------------------
+Drop Table [dbo].[TPA_Transit_Stops_2016_Build]
+Drop Table [dbo].[TPA_TRANSIT_STOPS]
+Drop Table [dbo].TPA_Stops_2016_Draft
+
+--Check Distance_Eligible and Meets_Headway_Criteria values for Bus to determine if the Distance based calculation used in SQL is working appropriately.
+------------------------------------------------------------
+Print 'End of Query' ---EOF 
+------------------------------------------------------------
+--Check Output
+--select * From [dbo].[rtd_route_stop_all_other_modes] --No output
+--select * from [dbo].[rtd_route_stop_schedule] -- No Output
+--select * From [dbo].[rtd_route_trips] --no output
+--select * From [dbo].[TPA_Transit_Stops_2016_Draft] where route_type <>'Bus' order by agency_id, route_id, route_type, stop_name -- Only Planned Transit
