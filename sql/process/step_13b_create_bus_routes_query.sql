2987d397dec3560c13e935dc24a3f9f0e830ad58
Tom Buckley <buckley.tom@gmail.com>
2017-05-10 13:12:06 -0700

    more table name clarifications

--- sql/process/step_13b_create_bus_routes_query.sql
+++ sql/process/step_13b_create_bus_routes_query.sql
@@ -3,16 +3,16 @@ SELECT        rtd_route_trips.agency_id, rtd_route_trips.agency_name, rtd_route_
                          stops.stop_lat, stops.stop_lon, calendar.monday, calendar.tuesday, calendar.wednesday, calendar.thursday, calendar.friday, 
                          calendar.agency_service_id, rtd_route_trips.route_short_name, rtd_route_trips.trip_headsign, rtd_route_trips.trip_id, stop_times.agency_trip_id, 
                          geometry::Point(stops.stop_lon, stops.stop_lat, 4326) AS SHAPE
-INTO              Route_Pattern_Bus_Stop_Schedule
+INTO              stops_bus_route_pattern_schedule
 FROM            stops INNER JOIN
                          stop_times ON stops.agency_stop_id = stop_times.agency_stop_id INNER JOIN
                          rtd_route_trips ON stop_times.agency_trip_id = rtd_route_trips.agency_trip_id INNER JOIN
                          calendar ON rtd_route_trips.agency_service_id = calendar.agency_service_id
 WHERE        (rtd_route_trips.route_type = 3)
 
-alter table Route_Pattern_Bus_Stop_Schedule
+alter table stops_bus_route_pattern_schedule
 add OBJECTID int IDENTITY(1,1) NOT NULL,
-CONSTRAINT [PK_Route_Pattern_Bus_Stop_Schedule] PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_stops_bus_route_pattern_schedule] PRIMARY KEY CLUSTERED 
 (
 	OBJECTID ASC
 ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
@@ -25,16 +25,16 @@ SELECT
      trip_headsign, 
      agency_id + ' - ' + route_id + ' - ' + Case When direction_id = 0 Then 'Inbound' Else 'Outbound' End as Agency_Route_Pattern, 
      geometry::Point(stop_lon, stop_lat, 4326) AS SHAPE
-INTO Route_Pattern_Bus_Stops
-FROM Route_Pattern_Bus_Stop_Schedule
+INTO stops_bus_route_pattern
+FROM stops_bus_route_pattern_schedule
 GROUP BY agency_id, agency_name, route_id, direction_id, stop_name, 
          stop_sequence, agency_stop_id, stop_lat, 
          stop_lon, route_short_name, trip_headsign
 ORDER BY agency_id, route_id, direction_id, stop_sequence
 
-alter table Route_Pattern_Stops
+alter table stops_bus_route_pattern
 add OBJECTID int IDENTITY(1,1) NOT NULL,
-CONSTRAINT [PK_Route_Pattern_Stop] PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_stops_bus_route_pattern] PRIMARY KEY CLUSTERED 
 (
 	OBJECTID ASC
 ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
