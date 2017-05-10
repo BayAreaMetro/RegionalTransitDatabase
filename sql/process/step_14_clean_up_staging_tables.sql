d3f571449f7f9cc595fee0bf0af1eb46c0e4cf52
Tom Buckley <buckley.tom@gmail.com>
2017-05-10 13:28:58 -0700

    rename:TPA_TRANSIT_STOPS-->stops_tpa_staging_headway_base_calculations
    renaming a table to fit into the table naming schema for gtfs and the project

--- sql/process/step_14_clean_up_staging_tables.sql
+++ sql/process/step_14_clean_up_staging_tables.sql
@@ -3,7 +3,7 @@
 Print 'Cleanup unneeded tables'
 ------------------------------------------------------------
 Drop Table [dbo].[stops_tpa_staging]
-Drop Table [dbo].[TPA_TRANSIT_STOPS]
+Drop Table [dbo].[stops_tpa_staging_headway_base_calculations]
 Drop Table [dbo].TPA_Stops_2016_Draft
 
 --Check Distance_Eligible and Meets_Headway_Criteria values for Bus to determine if the Distance based calculation used in SQL is working appropriately.
@@ -14,4 +14,4 @@ Print 'End of Query' ---EOF
 --select * From [dbo].[rtd_route_stop_all_other_modes] --No output
 --select * from [dbo].[rtd_route_stop_schedule] -- No Output
 --select * From [dbo].[rtd_route_trips] --no output
---select * From [dbo].[TPA_Transit_Stops_2016_Draft] where route_type <>'Bus' order by agency_id, route_id, route_type, stop_name -- Only Planned Transit
+--select * From [dbo].[stops_tpa_staging_headway_base_calculations_2016_Draft] where route_type <>'Bus' order by agency_id, route_id, route_type, stop_name -- Only Planned Transit
--- sql/process/step_5_insert_weekday_am_pm_headway_into_single_table.sql
+++ sql/process/step_5_insert_weekday_am_pm_headway_into_single_table.sql
@@ -1,18 +1,18 @@
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-Print 'Step 5. Insert Weekday (Monday thru Friday) AM/PM Peak Headway values into a container for summarization (TPA_TRANSIT_STOPS table).'
+Print 'Step 5. Insert Weekday (Monday thru Friday) AM/PM Peak Headway values into a container for summarization (stops_tpa_staging_headway_base_calculations table).'
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 GO
-	IF EXISTS(select * FROM sys.tables where name = 'TPA_TRANSIT_STOPS')
+	IF EXISTS(select * FROM sys.tables where name = 'stops_tpa_staging_headway_base_calculations')
 		begin
-			DROP TABLE TPA_TRANSIT_STOPS 
-			PRINT 'Dropping Table: TPA_TRANSIT_STOPS'
+			DROP TABLE stops_tpa_staging_headway_base_calculations 
+			PRINT 'Dropping Table: stops_tpa_staging_headway_base_calculations'
 		end
 ELSE
 	PRINT 'Table Does Not Exist';
 GO
-	Print 'Creating Table TPA_TRANSIT_STOPS'
+	Print 'Creating Table stops_tpa_staging_headway_base_calculations'
 GO
-	CREATE TABLE [TPA_TRANSIT_STOPS](
+	CREATE TABLE [stops_tpa_staging_headway_base_calculations](
 	RecID int IDENTITY(1,1) NOT NULL,	
 	[agency_id] [nvarchar](100) NULL,
 	[agency_name] [nvarchar](100) NULL,
@@ -36,18 +36,18 @@ GO
 ) ON [PRIMARY]
 GO
 	--------------------------------------------------------------------------------------------------------------------
-	Print 'Insert Weekday (Monday thru Friday) AM/PM Peak Headway values into the TPA_TRANSIT_STOPS table.'
+	Print 'Insert Weekday (Monday thru Friday) AM/PM Peak Headway values into the stops_tpa_staging_headway_base_calculations table.'
 	--------------------------------------------------------------------------------------------------------------------
 GO
 	Print 'Inserting Monday Values'
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_AM_Trips, Min_AM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max AM Trips], [Min AM Headway], 0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Monday_AM_Peak_Trips_15min_or_Less
 	Where [Max AM Trips] is not null
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_PM_Trips, Min_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max PM Trips], [Min PM Headway],0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Monday_PM_Peak_Trips_15min_or_Less
@@ -56,19 +56,19 @@ GO
 GO
 	Print 'Update the container table for each Weekday Value (Monday)'
 GO
-	Update [TPA_TRANSIT_STOPS]
+	Update [stops_tpa_staging_headway_base_calculations]
 	Set Weekday = 'Monday'
 	Where Weekday is null
 GO
 	Print 'Inserting Tuesday Values'
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_AM_Trips, Min_AM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max AM Trips], [Min AM Headway], 0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Tuesday_AM_Peak_Trips_15min_or_Less
 	Where [Max AM Trips] is not null
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_PM_Trips, Min_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max PM Trips], [Min PM Headway],0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Tuesday_PM_Peak_Trips_15min_or_Less
@@ -77,19 +77,19 @@ GO
 GO
 	Print 'Update the container table for each Weekday Value (Tuesday)'
 GO
-	Update [TPA_TRANSIT_STOPS]
+	Update [stops_tpa_staging_headway_base_calculations]
 	Set Weekday = 'Tuesday'
 	Where Weekday is null
 GO
 Print 'Inserting Wednesday Values'
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_AM_Trips, Min_AM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max AM Trips], [Min AM Headway], 0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM        #Wednesday_AM_Peak_Trips_15min_or_Less
 	Where [Max AM Trips] is not null
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_PM_Trips, Min_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max PM Trips], [Min PM Headway],0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Wednesday_PM_Peak_Trips_15min_or_Less
@@ -98,19 +98,19 @@ GO
 GO
 	Print 'Update the container table for each Weekday Value (Wednesday)'
 GO
-	Update [TPA_TRANSIT_STOPS]
+	Update [stops_tpa_staging_headway_base_calculations]
 	Set Weekday = 'Wednesday'
 	Where Weekday is null
 GO
 Print 'Inserting Thursday Values'
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_AM_Trips, Min_AM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max AM Trips], [Min AM Headway], 0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Thursday_AM_Peak_Trips_15min_or_Less
 	Where [Max AM Trips] is not null
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_PM_Trips, Min_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max PM Trips], [Min PM Headway],0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM        #Thursday_PM_Peak_Trips_15min_or_Less
@@ -119,19 +119,19 @@ GO
 GO
 	Print 'Update the container table for each Weekday Value (Thursday)'
 GO
-	Update [TPA_TRANSIT_STOPS]
+	Update [stops_tpa_staging_headway_base_calculations]
 	Set Weekday = 'Thursday'
 	Where Weekday is null
 GO
 Print 'Inserting Friday Values'
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_AM_Trips, Min_AM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max AM Trips], [Min AM Headway], 0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Friday_AM_Peak_Trips_15min_or_Less
 	Where [Max AM Trips] is not null
 GO
-	INSERT INTO [TPA_TRANSIT_STOPS]
+	INSERT INTO [stops_tpa_staging_headway_base_calculations]
 							 (agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Max_PM_Trips, Min_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat)
 	SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, [Max PM Trips], [Min PM Headway],0 as Delete_Stop, TPA, 0 as Meets_Headway_Criteria, 'Existing Transit Stop' as Stop_Description, null as Project_Description, stop_lon, stop_lat
 	FROM         #Friday_PM_Peak_Trips_15min_or_Less
@@ -140,7 +140,7 @@ GO
 GO
 	Print 'Update the container table for each Weekday Value (Friday)'
 GO
-	Update [TPA_TRANSIT_STOPS]
+	Update [stops_tpa_staging_headway_base_calculations]
 	Set Weekday = 'Friday'
 	Where Weekday is null
 GO
