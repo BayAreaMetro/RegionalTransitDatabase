1981c63e78736408ed214f4440b825e8b8fe7589
Tom Buckley <buckley.tom@gmail.com>
2017-05-11 15:58:16 -0700

    readability change

--- sql/process/stop_6_calculate_average_headways.sql
+++ sql/process/stop_6_calculate_average_headways.sql
@@ -14,12 +14,19 @@ GO
   Print 'Creating Table stops_tpa_staging'
 GO
 
-SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, AVG(Max_AM_Trips) AS Avg_Weekday_AM_Trips, AVG(Min_AM_Headway) AS Avg_Weekday_AM_Headway, 
-                         AVG(Max_PM_Trips) AS Avg_Weekday_PM_Trips, AVG(Min_PM_Headway) AS Avg_Weekday_PM_Headway, Delete_Stop, TPA, Meets_Headway_Criteria, TPA_Eligible, Stop_Description, Project_Description, stop_lon, stop_lat
+SELECT   agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, 
+		AVG(Max_AM_Trips) AS Avg_Weekday_AM_Trips, 
+		AVG(Min_AM_Headway) AS Avg_Weekday_AM_Headway, 
+        AVG(Max_PM_Trips) AS Avg_Weekday_PM_Trips, 
+        AVG(Min_PM_Headway) AS Avg_Weekday_PM_Headway, 
+        Delete_Stop, TPA, Meets_Headway_Criteria, TPA_Eligible, 
+        Stop_Description, Project_Description, stop_lon, stop_lat
 into stops_tpa_staging
 FROM         stops_tpa_staging_headway_base_calculations
 --Where Avg_Weekday_AM_Headway is not null and Avg_Weekday_PM_Headway is not null
-GROUP BY agency_id, agency_name, route_id, agency_stop_id, stop_name, route_type, Delete_Stop, TPA_Eligible, TPA, Meets_Headway_Criteria, Stop_Description, Project_Description, stop_lon, stop_lat
+GROUP BY agency_id, agency_name, route_id, agency_stop_id, stop_name, 
+		route_type, Delete_Stop, TPA_Eligible, TPA, Meets_Headway_Criteria, 
+		Stop_Description, Project_Description, stop_lon, stop_lat
 --ORDER BY agency_id, route_id, agency_stop_id
 
 GO
