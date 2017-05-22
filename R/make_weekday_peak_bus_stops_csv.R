87a4515d3351b6b57a993912a933af590f0107d1
Tom Buckley <buckley.tom@gmail.com>
2017-05-22 16:37:01 -0700

    debugging some variable name assignments

--- R/make_weekday_peak_bus_stops_csv.R
+++ R/make_weekday_peak_bus_stops_csv.R
@@ -1,5 +1,5 @@
 #be sure to set the project path
-PROJECT_PATH <- setwd("/Users/tommtc/Documents/Projects/RegionalTransitDatabase")
+PROJECT_PATH <- setwd("C:/projects/RTD/RegionalTransitDatabase")
 
 GTFS_PATH <- paste0(PROJECT_PATH,"/data/05_2017_511_GTFS/",collapse="")
 R_HELPER_FUNCTIONS_PATH <- paste0(PROJECT_PATH,"/R/r511.R",collapse="")
@@ -15,9 +15,108 @@ library(dplyr)
 
 setwd(GTFS_PATH)
 
-results <- list()
+gtfs_obj <- import_gtfs("MA.zip", local=TRUE)
 
-for (txt in dir(pattern = ".zip$",full.names=TRUE,recursive=TRUE)){
+df_sr <- get_stops_by_route(gtfs_obj)
+df_sr <- fix_arrival_time(df_sr)
+print(names(df_sr))
+#make booleans into nicer names
+df_sr$direction_id[df_sr$direction_id == 0] <- "Outbound"
+df_sr$direction_id[df_sr$direction_id == 1] <- "Inbound"
+
+#filter Stops to AM Peak Bus Routes
+time_start <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
+                      "06:00:00"),collapse=" ")
+time_end <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
+                    "09:59:00"),collapse=" ")
+am_stops <- filter_by_time(df_sr,
+                           time_start,
+                           time_end)
+
+
+am_stops <- remove_duplicate_stops(am_stops) #multiple identical stop time at the same stop
+am_stops <- count_trips(am_stops)
+am_stops <- subset(am_stops, am_stops$Headways < 16)
+print(names(am_stops))
+am_routes <- get_routes(am_stops)
+#4H Drop Duplicate Columns from DF
+print(names(am_routes))
+
+if (!(is.data.frame(am_routes) && nrow(am_routes)==0)){
+  am_routes["Peak_Period"] <-"AM Peak"
+} else 
+{
+  am_routes$Peak_Period <-  am_routes$route_id
+}
+
+###########################################################################################
+# Section 5. Create PM Peak Headways from Weekday Trips
+time_start <- paste0(c(format(Sys.Date(), "%Y-%m-%d"),
+                       "15:00:00"),collapse=" ")
+
+time_end <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
+                    "18:59:00"),collapse=" ")
+pm_stops <- filter_by_time(df_sr,
+                           time_start,
+                           time_end)
+
+pm_stops <- remove_duplicate_stops(pm_stops) #multiple identical stop time at the same stop
+pm_stops <- count_trips(pm_stops)
+pm_stops <- subset(pm_stops, pm_stops$Headways < 16)
+pm_routes <- get_routes(pm_stops)
+#4H Drop Duplicate Columns from DF
+pm_routes <- pm_routes[-c(7:9)]
+
+if (!(is.data.frame(pm_routes) && nrow(pm_routes)==0)){
+  pm_routes["Peak_Period"] <-"PM Peak"
+} else 
+{
+  pm_routes$Peak_Period <-  pm_routes$route_id
+}
+
+###########################################################################################
+# Section 6. Build Weekday High Frequency Bus Service Dataset
+
+df_rt_hf <- join_high_frequency_routes_to_stops(am_stops,pm_stops,am_routes,pm_routes)
+
+###########################################################################################
+# Section 7. Build Weekday High Frequency Bus Service Stops for Route Building using NA Tools
+
+#clear out arrival time--not clear its necessary below and not typed correctly
+df_rt_hf$arrival_time <- NULL
+
+df<- list(df_sr,df_rt_hf)
+
+df_stp_rt_hf <- Reduce(inner_join,df) %>%
+  group_by(agency_id, route_id, direction_id, trip_id,Peak_Period, Route_Pattern_ID,
+           trip_headsign, stop_id, stop_sequence, Total_Trips, Headway, Peak_Period,
+           TPA_Criteria, stop_lon, stop_lat) %>%
+  select(agency_id, route_id, direction_id, trip_id, Route_Pattern_ID,
+         trip_headsign, stop_id, stop_sequence, Total_Trips,
+         Headway, Peak_Period, TPA_Criteria,
+         stop_lon, stop_lat) %>%
+  arrange(agency_id, route_id, direction_id,
+          trip_id, Peak_Period, stop_sequence)
+
+rm(df)
+
+#Select Distinct Records based upon Agency Route Direction values.  Removes stop ids from output.
+df_stp_rt_hf <- group_by(df_stp_rt_hf,
+                         agency_id, route_id, direction_id, Route_Pattern_ID,trip_headsign,
+                         stop_id, stop_sequence, Total_Trips, Headway, Peak_Period,
+                         TPA_Criteria, stop_lon, stop_lat) %>%
+  distinct(agency_id, route_id, direction_id, Route_Pattern_ID,
+           trip_headsign, stop_id, stop_sequence, Total_Trips,
+           Headway, Peak_Period, TPA_Criteria, stop_lon, stop_lat)
+
+#Remove select cols.
+df_stp_rt_hf <- df_stp_rt_hf[-c(1:13)]
+return(df_stp_rt_hf)
+}
+
+
+
+for (txt in dir(pattern = "MA.zip$",full.names=TRUE,recursive=TRUE)){
   result = tryCatch({
     operator_prefix <- strsplit(strsplit(txt, "/")[[1]][[2]],".zip")[[1]]
     gtfs_obj <- import_gtfs(txt, local=TRUE)
--- R/r511.R
+++ R/r511.R
@@ -14,89 +14,7 @@
 #document some of the Reduce calls
 #collapse the am and pm filtering to one function and call it
 
-get_peak_bus_route_stops <- function(gtfs_obj) {
-  df_sr <- get_stops_by_route(gtfs_obj)
-  df_sr <- fix_arrival_time(df_sr)
-  #make booleans into nicer names
-  df_sr$direction_id[df_sr$direction_id == 0] <- "Outbound"
-  df_sr$direction_id[df_sr$direction_id == 1] <- "Inbound"
-  
-  #filter Stops to AM Peak Bus Routes
-  time_start <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
-                        "06:00:00"),collapse=" ")
-  time_end <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
-                      "09:59:00"),collapse=" ")
-  am_stops <- filter_by_time(df_sr,
-                             time_start,
-                             time_end)
-  
-  am_stops <- remove_duplicate_stops(am_stops) #multiple identical stop time at the same stop
-  am_routes <- get_bus_service(am_stops) 
-  if (!(is.data.frame(am_routes) && nrow(am_routes)==0)){
-    am_routes["Peak_Period"] <-"AM Peak"
-  } else 
-  {
-    am_routes$Peak_Period <-  am_routes$route_id
-  }
-  
-  ###########################################################################################
-  # Section 5. Create PM Peak Headways from Weekday Trips
-  time_start <- paste0(c(format(Sys.Date(), "%Y-%m-%d"),
-                         "15:00:00"),collapse=" ")
-  
-  time_end <- paste(c(format(Sys.Date(), "%Y-%m-%d"),
-                      "18:59:00"),collapse=" ")
-  pm_stops <- filter_by_time(df_sr,
-                             time_start,
-                             time_end)
-  pm_stops <- remove_duplicate_stops(pm_stops)
-  pm_routes <- get_bus_service(pm_stops)
-  if (!(is.data.frame(pm_routes) && nrow(pm_routes)==0)){
-    pm_routes["Peak_Period"] <-"PM Peak"
-  } else 
-  {
-    pm_routes$Peak_Period <-  pm_routes$route_id
-  }
-  
-  ###########################################################################################
-  # Section 6. Build Weekday High Frequency Bus Service Dataset
-  
-  df_rt_hf <- join_high_frequency_routes_to_stops(am_stops,pm_stops,am_routes,pm_routes)
-  
-  ###########################################################################################
-  # Section 7. Build Weekday High Frequency Bus Service Stops for Route Building using NA Tools
-  
-  #clear out arrival time--not clear its necessary below and not typed correctly
-  df_rt_hf$arrival_time <- NULL
-  
-  df<- list(df_sr,df_rt_hf)
-  
-  df_stp_rt_hf <- Reduce(inner_join,df) %>%
-    group_by(agency_id, route_id, direction_id, trip_id,Peak_Period, Route_Pattern_ID,
-             trip_headsign, stop_id, stop_sequence, Total_Trips, Headway, Peak_Period,
-             TPA_Criteria, stop_lon, stop_lat) %>%
-    select(agency_id, route_id, direction_id, trip_id, Route_Pattern_ID,
-           trip_headsign, stop_id, stop_sequence, Total_Trips,
-           Headway, Peak_Period, TPA_Criteria,
-           stop_lon, stop_lat) %>%
-    arrange(agency_id, route_id, direction_id,
-            trip_id, Peak_Period, stop_sequence)
-  
-  rm(df)
-  
-  #Select Distinct Records based upon Agency Route Direction values.  Removes stop ids from output.
-  df_stp_rt_hf <- group_by(df_stp_rt_hf,
-                           agency_id, route_id, direction_id, Route_Pattern_ID,trip_headsign,
-                           stop_id, stop_sequence, Total_Trips, Headway, Peak_Period,
-                           TPA_Criteria, stop_lon, stop_lat) %>%
-    distinct(agency_id, route_id, direction_id, Route_Pattern_ID,
-             trip_headsign, stop_id, stop_sequence, Total_Trips,
-             Headway, Peak_Period, TPA_Criteria, stop_lon, stop_lat)
-  
-  #Remove select cols.
-  df_stp_rt_hf <- df_stp_rt_hf[-c(1:13)]
-  return(df_stp_rt_hf)
-}
+
 
 ######
 ##Custom Time Format Functions
@@ -166,7 +84,7 @@ remove_duplicate_stops <- function(rt_df){
   return(rt_df_out)
 }
 
-count_trips_by_route <- function(rt_df) {
+count_trips<- function(rt_df) {
   rt_df %>%
     group_by(agency_id,
              route_id,
@@ -176,10 +94,11 @@ count_trips_by_route <- function(rt_df) {
     count(stop_sequence) %>%
     mutate(Headways = round(240/n,0)) ->
     rt_df_out
+  colnames(rt_df_out)[colnames(rt_df_out)=="n"] <- "Trips"
   return(rt_df_out)
 }
 
-select_distinct_on_agency_route_direction <- function(rt_df) {
+get_routes <- function(rt_df) {
   group_by(rt_df,
            agency_id,
            route_id,
@@ -195,26 +114,6 @@ select_distinct_on_agency_route_direction <- function(rt_df) {
              Headway) ->
     rt_df_out
   return(rt_df_out)
-}
-
-get_bus_service <- function(df_in,max_hdwy=16) {
-  #4C
-  df2 <- count_trips_by_route(df_in)
-
-  #4D Rename count col. (n) to Trips
-  names(df2)[6]<-"Trips"
-
-  #4E Select High Frequency Bus Service Routes (15 min or better headways)
-  df3 <- subset(df2,
-                df2$Headways < max_hdwy)
-
-  #4F Select Distinct Records based upon Agency Route Direction values.  Removes stop ids from output.
-  df_out <- select_distinct_on_agency_route_direction(df3)
-
-
-  #4H Drop Duplicate Columns from DF
-  df_out <- df_out[-c(7:9)]
-  return(df_out)
 }
 
 join_high_frequency_routes_to_stops <- function(am_stops,pm_stops,am_routes,pm_routes){
