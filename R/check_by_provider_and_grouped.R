e013700e80c96ce97177377b0bae2100e66025dd
Tom Buckley <buckley.tom@gmail.com>
2017-05-24 20:03:12 -0700

    compare route id, headways, counts for 2 methods

--- R/check_by_provider_and_grouped.R
+++ R/check_by_provider_and_grouped.R
@@ -1,18 +1,54 @@
-p1 <- "~/Documents/Projects/rtd1/data/05_2017_511_GTFS"
-p2 <- "~/Documents/Projects/RegionalTransitDatabase/data/05_2017_511_GTFS"
+library(readr)
+d1 <- "~/Documents/Projects/rtd/data/05_2017_511_GTFS"
+setwd(d1)
 
-setwd(p2)
-df1 = NULL
-for (txt in dir(pattern = "_peak_bus_routes.csv$",full.names=TRUE,recursive=TRUE)){
-  df1 = rbind(df1, read_csv(txt))
-}
+df_bp <- read_csv("Weekday_Peak_Routes_Builder.csv")
+df_all <- read_csv("Weekday_Peak_Bus_Routes_Stops_Builder.csv")
 
-df1 <- df
-setwd(p1)
+#make the route ID's match
+library(stringr)
+df_all$Route_Pattern_ID <- str_replace(df_all$Route_Pattern_ID,"Outbound", "0")
+df_all$Route_Pattern_ID <- str_replace(df_all$Route_Pattern_ID,"Inbound", "1")
 
-df2 <- read_csv("Weekday_Peak_Bus_Routes_Stops_Builder.csv")
+r_bp <- names(table(df_bp$Route_Pattern_ID))
+r_all <- names(table(df_all$Route_Pattern_ID))
 
-r1 <- table(df1$Route_Pattern_ID)
-r2 <- table(df2$Route_Pattern_ID)
+#table of routes in all-providers method not in by-provider method
+length(r_all[!(r_all %in% r_bp)])
+r_all[!(r_all %in% r_bp)]
 
-r2[!(r2 %in% r1)]
+#table of routes in the by-provider method not in the all-providers method
+length(r_bp[!(r_bp %in% r_all)])
+r_bp[!(r_bp %in% r_all)]
+
+df_bp_back <- df_bp
+df_all_back <- df_all
+
+#compare the headways
+df_bp <- df_bp_back[c('Route_Pattern_ID','Headway')]
+df_all <- df_all_back[c('Route_Pattern_ID','Headway')]
+
+rp_g <- group_by(df_bp, Route_Pattern_ID)
+df_bp_hdwy <- summarize(rp_g, mean(Headway))
+
+ra_g <- group_by(df_all, Route_Pattern_ID)
+df_all_hdwy <- summarize(ra_g, mean(Headway))
+
+cmpr_hdwy <- inner_join(df_bp_hdwy, df_all_hdwy, by = "Route_Pattern_ID", copy = FALSE, suffix = c(".bp", ".all"))
+
+print(cmpr_hdwy)
+                        
+#compare the counts
+df_bp <- df_bp_back[c('Route_Pattern_ID','Total_Trips')]
+df_all <- df_all_back[c('Route_Pattern_ID','Total_Trips')]
+
+rp_g <- group_by(df_bp, Route_Pattern_ID)
+df_bp_trps <- summarize(rp_g, mean(Total_Trips))
+
+ra_g <- group_by(df_all, Route_Pattern_ID)
+df_all_trps <- summarize(ra_g, mean(Total_Trips))
+
+cmpr_trips <- inner_join(df_bp_trps, df_all_trps, by = "Route_Pattern_ID", copy = FALSE, suffix = c(".bp", ".all"))
+
+print(cmpr_trips)
+
--- R/readme.md
+++ R/readme.md
@@ -4,7 +4,7 @@ R Scripts for 511 TPA Processing
 ### File List  
 
 -  `check_by_provider_and_grouped.R`    
-used to compare the bus route route stops file output. ideally, this should check that trip counts, headways, and routes are identically identified by the by-provider method and the all-providers method of data processing.  
+Used to compare the bus route route stops file output. Use to check that trip counts, headways, and routes are identically identified by the by-provider method and the all-providers method of data processing.  It seems like there are slight differences with counts (by provider is higher). This difference is probably related to issue #20. By-provider also reports 10 more routes than those found in the all-provider method. by-provider is not missing any of the routes in the all-providers method.   
 -  `DownloadDatasetsFromAPI.R`        
 a stub for a future R script to replace the functionality of an existing Python script   
 -  `r511.R`     
