1319a8e22abdbf1f9b99be6e847a7906f12bbeb6
Tom Buckley <buckley.tom@gmail.com>
2017-08-25 18:27:04 -0700

    write to geojson;fix db thing

--- python/get_511_zips.py
+++ python/get_511_zips.py
@@ -19,28 +19,49 @@ year = datetime.datetime.now().strftime('%Y')
 source = 'mtc511cache'
 dbstring = "postgresql:///tmp_gtfs"
 
-def get_511_orgs_dict():
+
+def get_511_operators_dict():
 	import requests
 	import xmltodict
-	operator_url = "http://api.511.org/transit/operators?api_key={}&Format=XML".format(APIKEY)
+	operator_url = "http://api.511.operator/transit/operators?api_key={}&Format=XML".format(APIKEY)
 	j = requests.get(operator_url)
 	d = xmltodict.parse(j.content)
 	return d
 
 def get_511_gtfs_zip(private_code, apikey=APIKEY):
-	request_url = 'http://api.511.org/transit/datafeeds?api_key={}&operator_id={}'.format(apikey,private_code)	
+	request_url = 'http://api.511.operator/transit/datafeeds?api_key={}&operator_id={}'.format(apikey,private_code)	
 	return requests.get(request_url, stream=True) #todo: add error handling
 
-def get_org_acronyms_from_511(dictionary):
-	orgs_list = dictionary['siri:Siri']['siri:ServiceDelivery']['DataObjectDelivery']['dataObjects']['ResourceFrame']['organisations']['Operator']
-	org_acronyms = []
-	for org_acronym in orgs_list:
-		org_acronyms.append(org_acronym['PrivateCode'])
-	return(org_acronyms)
+def get_cached_gtfs_zip(url):
+	return requests.get(url, stream=True)
+
+def get_operator_acronyms_from_511(dictionary):
+	operators_list = dictionary['siri:Siri']['siri:ServiceDelivery']['DataObjectDelivery']['dataObjects']['ResourceFrame']['operatoranisations']['Operator']
+	operator_acronyms = []
+	for operator_acronym in operators_list:
+		operator_acronyms.append(operator_acronym['PrivateCode'])
+	return(operator_acronyms)
+
+def shp_to_js(shapefile_path):
+	import fiona
+	import fiona.crs
+	geojson_path = shapefile_path.replace('.shp','.geojson')
+	with fiona.drivers():
+		with fiona.open(shapefile_path) as source:
+			meta = source.meta
+			print(meta)
+			meta['schema']['geometry'] = 'Point'
+			meta['driver'] = 'GeoJSON'
+			meta['crs'] = fiona.crs.from_epsg(4326)
+			with fiona.open(geojson_path, 'w', **meta) as sink:
+					for f in source:
+					   sink.write(f)
+	return geojson_path
+
 
 def export_shapefiles(operator, filename):
-	filename1 = '{}-stops.csv'.format(filename)
-	filename2 = '{}-hops.csv'.format(filename)
+	filename1 = '{}-stops.shp'.format(filename)
+	filename2 = '{}-hops.shp'.format(filename)
 	shpexport = ['gtfsrun',dbstring,
 	'ShapefileExport',
 	'--feed_id={}'.format(operator),
@@ -48,56 +69,91 @@ def export_shapefiles(operator, filename):
 	'--stopshp={}'.format(filename1),
 	'--hopshp={}'.format(filename2)]
 	print(subprocess.call(shpexport))
+	if os.path.exists(filename1):
+		filename1 = filename1
+		filename1 = shp_to_js(filename1)
+	else:
+		filename1 = False
+	if os.path.exists(filename2):
+		filename2 = filename2
+		filename2 = shp_to_js(filename2)
+	else:
+		filename2 = False
 	return({'stopsfile':filename1,'hopsfile':filename2})
 
-def export_frequencies(operator, filename):
-	filename1 = '{}-freq.csv'.format(filename)
-	shpexport = ['gtfsrun',dbstring,
+def export_frequencies(operator, filename_freq):
+	filename_freq1 = '{}-freq.csv'.format(filename_freq)
+	freqexport = ['gtfsrun',dbstring,
 	'Frequencies',
-	'--cluster=1',
+	'--cluster=50',
 	'--samename=True',
-	'--csv=data/{}'.format(filename1)]
-	print(subprocess.call(shpexport))
-	return(filename1)
+	'--csv=data/{}'.format(filename_freq1)]
+	print(subprocess.call(freqexport))
+	if os.path.exists(filename_freq1):
+		return(filename_freq1)
+	else:
+		return(False)
 
-def process_one(org_zip,org):
-	operator=org
-	s3name = write_511_gtfs_to_s3(org_zip)
+def process_one(dao,operator_zip,operator):
+	s3dict = {}
 	try:
-		dao = Dao(dbstring)
-		dao.load_gtfs(org_zip,feed_id=operator)
-		operator_filename = '{}/{}-{}'.format(path,timestamp,operator)
+		dao.load_gtfs(operator_zip,feed_id=operator)
+		print("loaded to db")
+	except Exception as e:
+		print(e)
+		try:
+			try: 
+				dao = Dao(dbstring)
+			except: 
+				next
+			dao.delete_feed(operator)
+		except:
+			next
+	try:
+		operator_filename = '{}/{}-{}'.format(working_dir,timestamp,operator)
 		filedict = export_shapefiles(operator,operator_filename)
 		filedict["frequencies"] = export_frequencies(operator,operator_filename)
 		dao.delete_feed(operator)
-		s3dict = {key:write_511_gtfs_to_s3(value) 
+		s3dict = {key:write_to_s3(value) if value else "na"
 					for key, value 
 					in filedict.items()}
-		s3dict["s3pathname"] = s3name
+		s3dict["s3pathname"] = ""
 		s3dict["processed"] = 1
-	except:
+	except Exception as e:
+		try: 
+			try: 
+				dao = Dao(dbstring), 
+			except: 
+				next
+			dao.delete_feed(operator)
+		except:
+			next
+		print(e)
 		s3dict = {"processed":0}
 		s3dict["frequencies"] = ""
 		s3dict["stopsfile"] = ""
 		s3dict["hopsfile"] = ""
-		s3dict["s3pathname"] = s3name
-	os.remove(org_zip)
+		s3dict["s3pathname"] = ""
+	try:
+		dao.delete_feed(operator)
+	except:
+		next
 	return(s3dict)
 
-def process_org(org, path = "."):
-	org_zip = '{}/{}-{}.zip'.format(path,timestamp,org)
-	if not os.path.exists(os.path.dirname(org_zip)):
-		os.makedirs(os.path.dirname(org_zip))
-	if not os.path.exists(org_zip):
-		r = get_511_gtfs_zip(org)
-		write_511_gtfs_to_disk(r, org_zip)
-		if os.path.exists(org_zip):
-			d = process_one(org_zip,org)
+def process_operator(dao,operator, url, path = "."):
+	operator_zip = '{}/{}-{}.zip'.format(path,timestamp,operator)
+	if not os.path.exists(os.path.dirname(operator_zip)):
+		os.makedirs(os.path.dirname(operator_zip))
+	if not os.path.exists(operator_zip):
+		r = get_cached_gtfs_zip(url)
+		write_zip_to_disk(r, operator_zip)
+		if os.path.exists(operator_zip):
+			d = process_one(dao,operator_zip,operator)
 			return(d)
 		else: 
 			return("nodata")
 			
-def write_511_gtfs_to_disk(r, path):
+def write_zip_to_disk(r, path):
 	import shutil
 	if r.status_code == 200:
 		with open(path, 'wb') as f:
@@ -112,34 +168,47 @@ def upload_file_from_local_511(transfer_filename,k):
     k.make_public()
     return(s3name)
 
-def write_511_gtfs_to_s3(filename):
-	aws_connection = S3Connection(AWS_KEY, AWS_SECRET)
-	bucket = aws_connection.get_bucket('mtc511gtfs')
-	k = Key(bucket)
-	s3name = upload_file_from_local_511(filename,k)
+def write_to_s3(filename):
+	print("writing to s3:" + filename)
+	try:
+		aws_connection = S3Connection(AWS_KEY, AWS_SECRET)
+		bucket = aws_connection.get_bucket('mtc511gtfs')
+		k = Key(bucket)
+		s3name = upload_file_from_local_511(filename,k)
+	except Exception as e:
+		print(e)
+		s3name = "na"
 	return(s3name)
 
 def main():
-	d = get_511_orgs_dict()
-	org_acronyms = get_org_acronyms_from_511(d)
+#	d = get_511_operators_dict()
+#	operator_acronyms = get_operator_acronyms_from_511(d)
 	df = pd.read_csv('data/cached_gtfs.csv')
 	df["frequencies"] = ""
 	df["stopsfile"] = ""
 	df["hopsfile"] = ""
-	for org in org_acronyms:
-		print("fetching:" + org)
-		d1 = process_org(org)
-		d = {'s3pathname': "https://s3-us-west-2.amazonaws.com/mtc511gtfs/"+d1['s3pathname'],
-			'frequencies': d1['frequencies'],
-			'stopsfile': d1['stopsfile'],
-			'hopsfile': d1['hopsfile'],
-			'operator': org,
-			'year': year,
-			'date_exported': date,
-			'source': source,
-			'processed':d1['processed'],
-			'filename':os.path.basename(d1['s3pathname'])}
-		df = df.append(d,ignore_index=True)
+	operator_urls = list(df.s3pathname)
+	operator_names = list(df.operator)
+	dao = Dao(dbstring)
+	for idx, url in enumerate(operator_urls):
+		operator = operator_names[idx]
+		print("fetching:" + operator)
+		d1 = process_operator(dao, operator, url)
+		print(d1)
+		if len(d1)>0:
+			d = {'s3pathname': "https://s3-us-west-2.amazonaws.com/mtc511gtfs/"+d1['s3pathname'],
+				'frequencies': d1['frequencies'],
+				'stopsfile': d1['stopsfile'],
+				'hopsfile': d1['hopsfile'],
+				'operator': operator,
+				'year': year,
+				'date_exported': date,
+				'source': source,
+				'processed':d1['processed'],
+				'filename':os.path.basename(d1['s3pathname'])}
+			df = df.append(d,ignore_index=True)
+		else:
+			next
 	df.to_csv('data/cached_gtfs2.csv')
 
 if __name__ == "__main__":
